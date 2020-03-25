547640e5d9af77126fed3be7fe687d0852eb00b2
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-03-25 22:31:47 +0200

    Move everything to 2020 folder

---
+++ 2020/lähtölaskenta/index.php
@@ -0,0 +1,58 @@
+<?php header('Content-type: text/html; charset=utf-8'); ?><!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta property="og:title" content="Wappukukka 2020" />
+	<title>Wappukukka 2020 Countdown</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<header>
+		<h1>Wappukukka 2020</h1>
+	</header>
+	<div id="maincontainer">
+		<div class="info normal">Aikaa jäljellä julkistamiseen</div>
+		<div id="timer" class="timer big">0 h 0 min 0 s</div>
+		<br>
+		<br>
+		<!--<a href="https://athene.fi/wappukukka">siirry Wappukukkaan</a>-->
+	</div>
+
+
+
+
+
+	<footer>
+		<div class="small">Johannes Vainion p&auml;ivitt&auml;m&auml; versio Atte Kein&auml;sen muokkaamasta Vesa Laakson Marathon-laskurista, jota kukaan ei ole päivittänyt ainakaan viimeiseen kolmeen vuoteen.</div>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script>
+        (function ($) {
+            var ts = parseInt(<?php echo (require "../launch_date.php")->getTimestamp(); ?>);
+            var releaseTime = new Date(ts * 1000).getTime();
+        var $timer = $('#timer');
+
+        function tick() {
+            var diff = releaseTime - new Date().getTime();
+
+            if (diff > 0) {
+                var secs = Math.floor(diff / 1000) % 60;
+                var mins = Math.floor(diff / 1000 / 60) % 60;
+                var hours = Math.floor(diff / 1000 / 60 / 60);
+
+                $timer.text(hours + " h " + mins + ' min ' + secs + ' s');
+            }
+            else {
+                window.location.reload();
+            }
+        }
+
+        setInterval(tick, 1000);
+        tick();
+
+        })(jQuery.noConflict());
+    </script>
+</body>
+</html>
