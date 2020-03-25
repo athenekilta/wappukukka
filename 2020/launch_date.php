3a740f60c32f2068fe034d3d5b53bd57ef8ca45c
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-03-25 22:23:07 +0200

    Change launch date when debug is true

---
+++ 2020/launch_date.php
@@ -0,0 +1,21 @@
+<?php
+# Checks current directory for year and assumes that
+# launch happens on first of April, so it should work
+# from year to year. Change anything if needed.
+# Time is ISO 8601 formatted, example:
+# 2020-04-01T00:00:00+03:00
+if (intval(file_get_contents("debug.txt")) == 1) {
+    return date_create(file_get_contents("debug_launch.txt"));
+}
+$current_year = basename(__DIR__);
+$launch_month = "04";
+$launch_date = "01";
+$hours = "00";
+$minutes = "00";
+$seconds = "00";
+$date_string = $current_year . "-" . $launch_month . "-" . $launch_date;
+$time_string = $hours . ":" . $minutes . ":" . $seconds;
+$time_difference = "+03:00";  # East European DST
+$date_object = date_create($date_string . "T" . $time_string . $time_difference);
+return $date_object;
+?>
