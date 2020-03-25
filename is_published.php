fe4d36b1dcb0b8bf6840f5a36cdb6db10b349c7e
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-03-25 22:40:31 +0200

    Remove the moved files actually

--- is_published.php
+++
@@ -1,6 +0,0 @@
-<?php
-$launch_date = require "launch_date.php";
-$current_date = new DateTime();
-$published = $current_date->getTimestamp() >= $launch_date->getTimestamp();
-return $published;
-?>
