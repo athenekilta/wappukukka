f07093fe7cbbc68c5412a4e95b16a91837e14b1a
Joel Juhela <joel.juhela@aalto.fi>
2020-03-28 18:34:25 +0200

    change front page layout and styles

--- 2020/etusivu.php
+++ 2020/etusivu.php
@@ -5,12 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Wappukukka 2020</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="etusivu.css" type="text/css">
-    <link rel="stylesheet" href="luukkulinkit.css" type="text/css">
+    <link rel="stylesheet" href="css/etusivu.css" type="text/css">
+    <link rel="stylesheet" href="css/luukkulinkit.css" type="text/css">
 </head>
 <body>
-    <h1>2020</h1>
-    
     <!-- alla php, joka generoi linkit gridiin !-->
     <?php include("luukkulinkit.php") ?>
     <!-- tämän php-tagin ylä- ja alapuolelle voi vapaasti laittaa omaa html:ää :) !-->
