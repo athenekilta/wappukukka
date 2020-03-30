894043cd237d21f90a144a1a59e9f967b8be5ac1
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-03-30 21:34:06 +0300

    Add todo list from previous versions

--- 2020/etusivu.php
+++ 2020/etusivu.php
@@ -10,7 +10,23 @@
 </head>
 <body>
     <!-- alla php, joka generoi linkit gridiin !-->
-    <?php include("luukkulinkit.php") ?>
+    <?php include("luukkulinkit.php") 
+              //TODO Todo
+              /*
+               * - toim. ilman js
+               * - asettelu toimimaan
+               * - atte on laiska eikä jaksa tehdä näille mitään vielä(kään) vuonna 2014
+               * - toivottavasti seuraaja on ahkerampi tapaus
+               * - Johanneskaan ei tee mitään 2015
+               * - ... t. Alexia ja Mari 2016-17 ps. Ilona lisäs vielä enemmän js höhöhöhö
+               * - Jos joku ei toimi nii se on noiden ylempien vika t. Vili 2018
+               * - En osaa k�ytt�� windows, mut ei osaa Alexia tai Ilonakaan. VJP tuli liian my�h��n t. Oliver vuonna 2019
+               * - nyt toimii ainakin asettelu ilman js (2020)
+               * - asettelu ilmeisesti toimii? Kiitos J. Juhela! (2020)
+               * - laitettiin vähän paikkoja uusiksi mutta sama purkka täällä edelleen pitää hommaa kasassa!
+               * - mvh srapeli 30.3.2020
+               */
+    ?>
     <!-- tämän php-tagin ylä- ja alapuolelle voi vapaasti laittaa omaa html:ää :) !-->
 </body>
 </html>
