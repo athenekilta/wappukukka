b7ac8ce7096014c31d750140982ce689178ec21c
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2020-03-30 16:01:13 +0300

    Update config for production

--- 2020/config.php
+++ 2020/config.php
@@ -1,8 +1,9 @@
 <?php
+# For production, set debug => false, show_all_luukkus => false and test_launch => false
 return array(
-    'debug' => true,
+    'debug' => false,
     'show_all_luukkus' => false,
-    'test_launch' => true,
+    'test_launch' => false,
     'test_launch_date' => date_create("2020-03-25T21:41:00+02:00")
 );
 ?>
